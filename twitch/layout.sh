bf105675d63333eec85b032980fb5bd2351c8dbb
Soothspider <soothspider@proton.me>
2023-11-25 16:07:50 -0800

    Quick caption fixes to 3 streams

---
+++ twitch/layout.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/bash
+
+# ---
+# Given my normal naming scheme, creates a properly named folder, 
+# moves the vtt into it and renames everything.
+# 
+# Normally something like: 
+# - <twitchId> - name [gigaohmbiological - <date>].vtt
+# - <twitchId>,<twitchId22> - name [gigaohmbiological - <date>].vtt
+#
+# Need to change it to something like:
+# - <twitchId> (<date>) - name.vtt
+# ---
+
+files=()
+while (( "$#" )); do
+	files+=("$1")
+	echo Adding file: "$1"
+	shift
+done
+
+for ((i = 0; i < ${#files[@]}; i++)); do
+    suffix=""
+	file="${files[$i]}"
+
+	echo processing: $file
+
+    base="${file%.*}"
+    [[ "$base" =~ .*"fixed"$ ]] && base="${base%.*}" && suffix=".fixed"
+    [[ "$base" =~ .*"joined"$ ]] && base="${base%.*}" && suffix=".joined"
+
+    id="$(cut -d' ' -f1 <<< "$base")"
+    date="$(echo $base | cut -d'[' -f2 | cut -d']' -f1 | cut -d' ' -f3)"
+    stream="$(echo $base | cut -d' ' -f3- | cut -d'[' -f1)"
+    stream="${stream/"Brief "/"Gigaohm Biological High Resistance Low Noise Information Brief"}"
+	folder="${id} (${date}) - ${stream}"
+	target="${folder}${suffix}.vtt"
+
+    [ ! -d "${folder}" ] && mkdir -v "${folder}"
+    mv -vi "${file}" "${folder}/${target}"
+
+    # printf "\n base: $base \n date: $date \n id: $id \n stream: $stream \n"
+    # printf " folder: $folder \n target: $target \n\n"
+
+done
+
